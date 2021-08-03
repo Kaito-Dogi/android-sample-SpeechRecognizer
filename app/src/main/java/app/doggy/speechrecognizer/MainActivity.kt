94f980ccb7f33d7c0e5d5da37d89f268871e9d4d
Kaito-Dogi <kendobu0405@gmail.com>
2021-08-03 21:04:04 +0900

    update: パーミッションを追加

--- app/src/main/java/app/doggy/speechrecognizer/MainActivity.kt
+++ app/src/main/java/app/doggy/speechrecognizer/MainActivity.kt
@@ -1,11 +1,27 @@
 package app.doggy.speechrecognizer
 
+import android.Manifest.permission.INTERNET
+import android.Manifest.permission.RECORD_AUDIO
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val PERMISSIONS_RECORD_AUDIO = 1000
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val permissionsGranted = ContextCompat.checkSelfPermission(this, RECORD_AUDIO)
+        if (permissionsGranted != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(RECORD_AUDIO), PERMISSIONS_RECORD_AUDIO)
+        }
+
     }
 }
